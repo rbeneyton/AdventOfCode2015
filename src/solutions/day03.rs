23b9960952d030a41ed1a133f2eaaba3b281553d
Richard Beneyton <richard.beneyton@gmail.com>
2022-12-02 19:52:48 +0100

    dayXX: add tests

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -14,3 +14,28 @@ pub fn solve(part: u8, input: &'static str) -> Solution {
         Solution::I64(0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_sample() {
+        assert_eq!(solve(1, r"..."), Solution::U64(0));
+    }
+
+    #[test]
+    fn part_1() {
+        assert_eq!(solve(1, ""), Solution::U64(0));
+    }
+
+    #[test]
+    fn part_2_sample() {
+        assert_eq!(solve(2, r"..."), Solution::U64(0));
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(solve(2, ""), Solution::U64(0));
+    }
+}
